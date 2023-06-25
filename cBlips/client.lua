1b77fb55bc0cb1e463e1e442d9131bb10a3bbae9
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2023-06-25 10:04:38 +0200

    Update client.lua

--- cBlips/client.lua
+++ cBlips/client.lua
@@ -1,14 +1,15 @@
 -- This part is the code for creating the blip, you can find the configuration part in the config.lua file!
 
 CreateThread(function()
-    for _, v in pairs(Config.Blip["Location"]) do
-        local blip = AddBlipForCoord(v.coords.x, v.coords.y, v.coords.z)
-        SetBlipSprite(blip, v.sprite)
-        SetBlipScale(blip, v.scale)
-        SetBlipColour(blip, v.colour)
-        SetBlipAsShortRange(blip, v.asshortrange)
+    for k,v in pairs(Config.Blips) do
+        local Blips = AddBlipForCoord(QBConfig.Blips[k]['X'], Config.Blips[k]['Y'], Config.Blips[k]['Z'])
+        SetBlipSprite(Blips, Config.Blips[k]['SpriteId'])
+        SetBlipDisplay(Blips, 4)
+        SetBlipScale(Blips, Config.Blips[k]['Scale'])
+        SetBlipAsShortRange(Blips, true)
+        SetBlipColour(Blips, Config.Blips[k]['Color'])
         BeginTextCommandSetBlipName("STRING")
-        AddTextComponentString(v.label)
-        EndTextCommandSetBlipName(blip)
+        AddTextComponentSubstringPlayerName(Config.Blips[k]['Name'])
+        EndTextCommandSetBlipName(Blips)
     end
 end)
